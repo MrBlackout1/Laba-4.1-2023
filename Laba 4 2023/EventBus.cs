a253a0672a2d3634fde3b0e090357d76afde63cb
MrBlackout1 <mrbobfish0@gmail.com>
2023-03-18 19:22:09 +0200

    first commit

---
+++ Laba 4 2023/EventBus.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_4_2023
+{
+    public class EventBus
+        {
+            private Dictionary<string, List<Delegate>> _eventHandlers;
+            private DateTime _lastEventTime;
+            private int _eventThrottleMilliseconds;
+            public EventBus(int eventThrottleMilliseconds)
+            {
+                _eventHandlers = new Dictionary<string, List<Delegate>>();
+                _eventThrottleMilliseconds = eventThrottleMilliseconds;
+                _lastEventTime = DateTime.MinValue;
+            }
+            public void RegisterEventHandler(string eventName, Delegate eventHandler)
+            {
+                if (!_eventHandlers.ContainsKey(eventName))
+                {
+                    _eventHandlers[eventName] = new List<Delegate>();
+                }
+                _eventHandlers[eventName].Add(eventHandler);
+            }
+            public void UnregisterEventHandler(string eventName, Delegate eventHandler)
+            {
+                if (_eventHandlers.ContainsKey(eventName))
+                {
+                    _eventHandlers[eventName].Remove(eventHandler);
+                }
+            }
+            public void RaiseEvent(string eventName, object sender, EventArgs args)
+            {
+                if (_eventHandlers.ContainsKey(eventName))
+                {
+                    var eventHandlers = _eventHandlers[eventName];                    
+                    var timeSinceLastEvent = DateTime.Now - _lastEventTime;
+                    if
+                        (timeSinceLastEvent.TotalMilliseconds < _eventThrottleMilliseconds)
+                    {
+                        var waitTime = _eventThrottleMilliseconds - timeSinceLastEvent.TotalMilliseconds;
+                        Thread.Sleep((int)waitTime);
+                    }
+                    foreach (var eventHandler in eventHandlers)
+                    {
+                        eventHandler.DynamicInvoke(sender, args);
+                    }
+                    _lastEventTime = DateTime.Now;
+                }
+            }
+        }
+    }
---
+++ Laba 4 2023/Program.cs
@@ -0,0 +1,41 @@
+﻿using Laba_4_2023;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var eventBus = new EventBus(2000);
+        
+        eventBus.RegisterEventHandler("event1", new EventHandler(EventHandler1));
+        eventBus.RegisterEventHandler("event1", new EventHandler(EventHandler2));
+        eventBus.RegisterEventHandler("event2", new EventHandler(EventHandler3));
+        
+        eventBus.RaiseEvent("event1", null, EventArgs.Empty);
+        eventBus.RaiseEvent("event2", null, EventArgs.Empty);
+        eventBus.RaiseEvent("event1", null, EventArgs.Empty);
+
+        eventBus.UnregisterEventHandler("event1", new EventHandler(EventHandler1));
+                
+        eventBus.RaiseEvent("event1", null, EventArgs.Empty);
+        eventBus.RaiseEvent("event2", null, EventArgs.Empty);
+        eventBus.RaiseEvent("event1", null, EventArgs.Empty);
+    }
+
+    static void EventHandler1(object sender, EventArgs args)
+    {
+        Console.WriteLine("EventHandler1 called");
+    }
+
+    static void EventHandler2(object sender, EventArgs args)
+    {
+        Console.WriteLine("EventHandler2 called");
+    }
+
+    static void EventHandler3(object sender, EventArgs args)
+    {
+        Console.WriteLine("EventHandler3 called");
+    }
+}
